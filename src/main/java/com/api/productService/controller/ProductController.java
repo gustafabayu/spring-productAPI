45e525fabd1067a34c41ae07fd623a6c0bd528bb
Gustafa Bayu <faafa13@gmail.com>
2023-07-25 15:41:17 +0700

    2nd commit

--- src/main/java/com/api/productService/controller/ProductController.java
+++ src/main/java/com/api/productService/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.api.productService.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +11,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.productService.dto.ProductRequest;
+import com.api.productService.dto.ProductResponse;
+import com.api.productService.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
